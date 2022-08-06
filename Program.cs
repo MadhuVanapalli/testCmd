c5a8368435384ee269e1a4f83e381286362f9b1e
Madhu <Madhu@DESKTOP-FGHO6HL>
2022-08-06 13:46:13 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace testCmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private bool IsPalindrome(int num)
+        {
+            bool isValid = false;
+
+
+            return isValid;
+        }
+        
+    }
+}
